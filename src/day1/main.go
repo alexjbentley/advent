4900b273460958607d109dfb6be0e3ece719b325
Alexander Bentley <alexjbentley@bentleyandco.force9.co.uk>
2021-12-16 21:45:01 +0000

    day1/part 2 complete

--- src/day1/main.go
+++ src/day1/main.go
@@ -8,7 +8,7 @@ import (
 	"utils"
 )
 
-func main() {
+func readDepths() []int {
 	data, err := os.ReadFile("files/depths.txt")
 	utils.Check(err)
 
@@ -19,12 +19,48 @@ func main() {
 		readings[i], err = strconv.Atoi(readingString)
 		utils.Check(err)
 	}
-	prevReading := 0
-	numIncreased := -1 // First will be a false positive
+	return readings
 
-	for _, reading := range readings {
-		if reading > prevReading {
-			numIncreased++
+}
+
+func sumWindow(window []int) int {
+	return window[0] + window[1] + window[2]
+}
+
+func main() {
+	readings := readDepths()
+	numIncreased := 0
+	leadWindow := make([]int, 0) // Window at the front
+	prevWindow := make([]int, 0) // Trailing window
+	prevReading := 0
+	for i, reading := range readings {
+		if i == 0 {
+			// First load, don't alter the trailing window
+			leadWindow = append(leadWindow, reading) // Push to lead
+		} else if i < 3 {
+			// We haven't loaded enough values to compare yet
+			leadWindow = append(leadWindow, reading)     // Push to lead
+			prevWindow = append(prevWindow, prevReading) // Push to trailing
+		} else if i == 3 {
+			// Special case, leadWindow will end up with 4 elements before the pop
+			leadWindow = append(leadWindow, reading)     // Push to lead
+			prevWindow = append(prevWindow, prevReading) // Push to trailing
+			leadWindow = leadWindow[1:]
+			if sumWindow(leadWindow) > sumWindow(prevWindow) {
+				numIncreased++
+			}
+		} else {
+			// Sliding windows are ready
+			// Push
+			leadWindow = append(leadWindow, reading)     // Push to lead
+			prevWindow = append(prevWindow, prevReading) // Push to trailing
+			leadWindow = leadWindow[1:]
+			prevWindow = prevWindow[1:]
+
+			// Compare
+			if sumWindow(leadWindow) > sumWindow(prevWindow) {
+				numIncreased++
+			}
 		}
 		prevReading = reading
 	}
