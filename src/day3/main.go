727bad1af71167780623e6c162795b67eeb83281
alexjbentley <alexjbentley@bentleyandco.force9.co.uk>
2021-12-18 18:21:19 +0000

    day3/part1 working, we forgot to consider the MSB being first character getting read.

--- src/day3/main.go
+++ src/day3/main.go
@@ -15,8 +15,8 @@ func getMask(length int) (mask int) {
 	return
 }
 
-func readReport() (bitCounts []int, reportLength int) {
-	data, err := os.ReadFile("files/diagnostic-report.txt")
+func readReport(filename string) (bitCounts []int, reportLength int) {
+	data, err := os.ReadFile(filename)
 	utils.Check(err)
 
 	// data is currently a []byte
@@ -25,14 +25,19 @@ func readReport() (bitCounts []int, reportLength int) {
 
 	// Let's get the length of the lines
 	numBits := len(reportLines[0])
+
+	// Intialise our slice to be zeroed
 	bitCounts = make([]int, numBits)
 	for i := 0; i < numBits; i++ {
 		bitCounts[i] = 0
 	}
 
+	// For each report line
 	for _, reportLine := range reportLines {
+		// Split into characters
 		lineRunes := strings.Split(reportLine, "")
-		for i := 0; i < numBits; i++ {
+		// Note that MSB will be read first!
+		for i := 0; i < len(lineRunes); i++ {
 			if lineRunes[i] == "1" {
 				bitCounts[i]++
 			}
@@ -43,16 +48,19 @@ func readReport() (bitCounts []int, reportLength int) {
 }
 
 func main() {
-	bitCounts, reportLength := readReport()
+	bitCounts, reportLength := readReport(os.Args[1])
 	gammaRate := 0b0
-	for i := 0; i < len(bitCounts); i++ {
+	numBits := len(bitCounts)
+
+	// Note that bitCounts[0] concerns MSB!
+	for i := 0; i < numBits; i++ {
 		if bitCounts[i] > reportLength/2 {
-			gammaRate = gammaRate | 0b1<<i
+			gammaRate = gammaRate | 0b1<<(numBits-i-1)
 		}
 	}
-	epsilonRate := gammaRate ^ getMask(len(bitCounts))
-	fmt.Printf("Gamma: %012b %d\n", gammaRate, gammaRate)
-	fmt.Printf("Epsilon: %012b %d\n", epsilonRate, epsilonRate)
+	epsilonRate := gammaRate ^ getMask(numBits)
+	fmt.Printf("Gamma: %012b (%d)\n", gammaRate, gammaRate)
+	fmt.Printf("Epsilon: %012b (%d)\n", epsilonRate, epsilonRate)
 
 	powerConsumption := gammaRate * epsilonRate
 	fmt.Println(powerConsumption)
